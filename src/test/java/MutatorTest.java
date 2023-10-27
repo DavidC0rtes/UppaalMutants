a68f63d04d5fda4571e18d50225717fc95072106
Santiago Cortes <david.cortes@correounivalle.edu.co>
2023-10-26 21:24:48 -0500

    Test croquis

---
+++ src/test/java/MutatorTest.java
@@ -0,0 +1,13 @@
+import Parser.Receiver.Mutator;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+public class MutatorTest {
+    private final Mutator mutator = new Mutator();
+    @Test
+    void testKilledData() {
+        // Arrange
+
+    }
+}
